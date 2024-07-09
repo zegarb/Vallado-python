675467cd1d1b0eea1d894e6e979b481757a8ca8c
zegarb <zgarbarino@gmail.com>
2024-07-09 09:03:26 -0400

    remove extraneous moonrise functions

--- ex5_4.py
+++ ex5_4.py
@@ -40,41 +40,25 @@ print('moon moonrise %14.6f  %14.4f   moonset %14.6f %14.4f  \n'
          utmoonset, (utmoonset - np.floor(utmoonset)) * 60))
 print('moon phase angle %14.4f   \n' % (moonphaseang))
 
-utmoonrise, utmoonset, moonphaseang, error = obu.moonrise2(jd + jdfrac, latgd,
-                                                           lon)
-print('2moon moonrise %14.6f  %14.4f   moonset %14.6f %14.4f  \n'
-      % (utmoonrise, (utmoonrise - np.floor(utmoonrise)) * 60,
-         utmoonset, (utmoonset - np.floor(utmoonset)) * 60))
-print('2moon phase angle %14.4f   \n' % (moonphaseang))
-
-# seems to be the most accurate. -zeg
-utmoonrise, utmoonset, moonphaseang, error = obu.moonrise3(jd + jdfrac, latgd,
-                                                           lon)
-print('3moon moonrise %14.6f  %14.4f   moonset %14.6f %14.4f  \n'
-      % (utmoonrise, (utmoonrise - np.floor(utmoonrise)) * 60,
-         utmoonset, (utmoonset - np.floor(utmoonset)) * 60))
-print('3moon phase angle %14.4f   \n' % (moonphaseang))
-
 jd, jdfrac = stu.jday(1990, 3, 5, 0, 0, 0.0)
 latgd = 40.94 * deg2rad
 lon = - 73.97 * deg2rad
-#        [utmoonrise, utmoonset, moonphaseang, error] = moonrise1( jd, latgd, lon, 'y' )
-#        fprintf(1, 'moon moonrise #14.4f  #14.4f   moonset #14.4f #14.4f  \n', utmoonrise, (utmoonrise-floor(utmoonrise))*60, utmoonset, (utmoonset-floor(utmoonset))*60 );
-#        fprintf(1, 'moon phase angle #14.4f   \n', moonphaseang );
-utmoonrise, utmoonset, moonphaseang, error = obu.moonrise2(jd + jdfrac, latgd,
-                                                           lon)
+utmoonrise, utmoonset, moonphaseang, error = obu.moonrise(jd + jdfrac, latgd,
+                                                          lon)
 print('moon moonrise %14.4f  %14.4f   moonset %14.4f %14.4f  \n'
       % (utmoonrise, (utmoonrise - np.floor(utmoonrise)) * 60,
          utmoonset, (utmoonset - np.floor(utmoonset)) * 60))
 print('moon phase angle %14.4f   \n' % (moonphaseang))
+
 jd, jdfrac = stu.jday(2006, 6, 28, 0, 0, 0.0)
 latgd = 40.0 * deg2rad
 lon = 0.0 * deg2rad
-utmoonrise, utmoonset, moonphaseang, error = obu.moonrise2(jd + jdfrac,
-                                                           latgd, lon)
+utmoonrise, utmoonset, moonphaseang, error = obu.moonrise(jd + jdfrac,
+                                                          latgd, lon)
 print('moon moonrise %14.4f    moonset %14.4f hrs \n'
       % (utmoonrise, utmoonset))
 print('moon phase angle %14.4f   \n' % (moonphaseang))
+
 jd = 2458291.0
 jdfrac = 0.0
 latgd = - 108.2802963256836 * deg2rad
@@ -94,29 +78,25 @@ for i in range(8, 31):
         latgd = (40.0 + j * 2.0) * deg2rad
         lon = 0.0 * deg2rad
         utmoonrise, utmoonset, moonphaseang, error = \
-            obu.moonrise3(jd + jdfrac, latgd, lon)
-        #                if strcmp(error, 'ok') == 0 # 1 if true, 0 if false
-#                    fprintf(1, 'error');
-#                end;
+            obu.moonrise(jd + jdfrac, latgd, lon)
+            # if strcmp(error, 'ok') == 0 # 1 if true, 0 if false
+                # fprintf(1, 'error');
         hr, min, sec = sc.rad2hms(utmoonrise * 15.0 * np.pi / 180.0)
         hr1, min1, sec1 = sc.rad2hms(utmoonset * 15.0 * np.pi / 180.0)
         #  print out header date for each section of results
         if j == 0:
             print('%2i ' % (i))
-        #         if utmoonrise > 9998.0 utmoonset > 9998.0
-#              fprintf(1, ' none ');
-#         else
-#         if utmoonrise > 9998.0 && utmoonset < 24.0
-#              fprintf(1, ' nors ');
-#         else
-# #        if utmoonset - utmoonrise > 14.0
-# #             fprintf(1, ' none ');
-# #        else
-#             fprintf(1, '#2i:#2i ', hr, min);
-# #        end;
-#         end;
-#         end;
-        #           if hr >= 24
+                # if utmoonrise > 9998.0 utmoonset > 9998.0
+            #  fprintf(1, ' none ');
+        # else
+        # if utmoonrise > 9998.0 && utmoonset < 24.0
+            #  fprintf(1, ' nors ');
+        # else
+        # if utmoonset - utmoonrise > 14.0
+            # fprintf(1, ' none ');
+        # else
+            # fprintf(1, '#2i:#2i ', hr, min);
+        # if hr >= 24
         #jd, jdfrac = stu.jday(2006, 6, i, 0, 0, 0.0)
         #el1 = obu.moonel(jd + jdfrac, latgd, lon)
         #jd, jdfrac = stu.jday(2006, 6, i + 1, 0, 0, 0.0)
@@ -134,10 +114,10 @@ for i in range(8, 31):
         latgd = (40.0 + j * 2.0) * deg2rad
         lon = 0.0 * deg2rad
         utmoonrise, utmoonset, moonphaseang, error = \
-            obu.moonrise3(jd + jdfrac, latgd, lon)
-        #                if strcmp(error, 'ok') == 0 # 1 if true, 0 if false
-#                    fprintf(1, 'error');
-#                end;
+            obu.moonrise(jd + jdfrac, latgd, lon)
+            # if strcmp(error, 'ok') == 0 # 1 if true, 0 if false
+                # fprintf(1, 'error');
+            # end;
         hr, min, sec = sc.rad2hms(utmoonrise * 15.0 * np.pi / 180.0)
         hr1, min1, sec1 = sc.rad2hms(utmoonset * 15.0 * np.pi / 180.0)
         #  print out header date for each section of results
