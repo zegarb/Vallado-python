e5b5ecf54bb6525346a160c78224bce44dea1dbb
CourvilleM <michaelcourville13@gmail.com>
2024-08-01 17:11:15 -0400

    ex 11-7/riset book tests

--- ex11_7.py
+++ ex11_7.py
@@ -19,54 +19,55 @@ def azse_signcheck(azlim_low, azlim_high, sez_array, highlow):
     if azlim_low < 0.0:
         azlim_low + 2 * math.pi
 
-    if highlow == 'h':
+    if highlow == 'high':
         azlim = azlim_high
-    elif highlow == 'l':
+    elif highlow == 'low':
         azlim = azlim_low
     else:
         print('INVALID OPTION')
         return None
-    
-    # 180 degree seperation between azimuth 
-    # high and low will always produce valid roots
-    if (math.pi - np.abs(azlim_high - azlim_low)) < small:
-        valid_root = True
-    else:
-        # North Axis
-        if (0.0 - azlim) < small or (2*math.pi - azlim) < small:
-            az_southsign = -1
-            az_eastsign = 0
-        # East Axis
-        elif (math.pi/2 - azlim) < small:
-            az_southsign = 0
-            az_eastsign = 1
-        # South Axis
-        elif (math.pi - azlim) < small:
-            az_southsign = 1
-            az_eastsign = 0
-        # West Axis
-        elif (3*math.pi/2 - azlim) < small:
-            az_southsign = 0
-            az_eastsign = -1
-        # Quadrant 1
-        elif azlim > 0.0 and azlim < math.pi/2:
-            az_southsign = -1
-            az_eastsign = 1
-        # Quadrant 2
-        elif azlim > math.pi/2 and azlim < math.pi:
-            az_southsign = 1
-            az_eastsign = 1
-        # Quadrant 3
-        elif azlim > math.pi and azlim < 3*math.pi/2:
-            az_southsign = 1
-            az_eastsign = -1
-        # Quadrant 4
-        elif azlim >  3*math.pi/2 and azlim < 2*math.pi:
-            az_southsign = -1
-            az_eastsign = -1
 
-        if (sez_sign[0] + az_southsign) != 0 and (sez_sign[1] + az_eastsign) != 0:
-            valid_root = True
+    # North Axis
+    if np.abs(0.0 - azlim) < small or np.abs(2*math.pi - azlim) < small:
+        az_southsign = -1
+        az_eastsign = 0
+    # East Axis
+    elif np.abs(math.pi/2 - azlim) < small:
+        az_southsign = 0
+        az_eastsign = 1
+    # South Axis
+    elif np.abs(math.pi - azlim) < small:
+        az_southsign = 1
+        az_eastsign = 0
+    # West Axis
+    elif np.abs(3*math.pi/2 - azlim) < small:
+        az_southsign = 0
+        az_eastsign = -1
+    # Quadrant 1
+    elif azlim > 0.0 and azlim < math.pi/2:
+        az_southsign = -1
+        az_eastsign = 1
+    # Quadrant 2
+    elif azlim > math.pi/2 and azlim < math.pi:
+        az_southsign = 1
+        az_eastsign = 1
+    # Quadrant 3
+    elif azlim > math.pi and azlim < 3*math.pi/2:
+        az_southsign = 1
+        az_eastsign = -1
+    # Quadrant 4
+    elif azlim >  3*math.pi/2 and azlim < 2*math.pi:
+        az_southsign = -1
+        az_eastsign = -1
+
+    # If angle falls on south axis only use east component to check angle validity
+    if az_southsign == 0 and (sez_sign[1] + az_eastsign) != 0:
+        valid_root = True
+    # If angle falls on east axis only use south component to check angle validity
+    elif az_eastsign == 0 and (sez_sign[0] + az_southsign) != 0:
+        valid_root = True
+    elif (sez_sign[0] + az_southsign) != 0 and (sez_sign[1] + az_eastsign) != 0:
+        valid_root = True
 
     return valid_root
 
@@ -88,7 +89,7 @@ def riset(rsat_eci_initial, vsat_eci_initial, latgd, lon, hellp, jd, dut1, dat, 
     rhosite_sez, _ = sc.ecef2sez(rsite_ecef, vsite_ecef, latgd, lon)
 
     # Size: 1440 min in a day / 3 min (180sec) intervals
-    size = int(1440/3) + 5
+    size = int(1440/1) + 5
     rsat_ecef_array = np.zeros((size,3))
     rhosat_sez_array = np.zeros((size,3))
     drhosat_sez_array = np.zeros((size,3))
@@ -104,8 +105,10 @@ def riset(rsat_eci_initial, vsat_eci_initial, latgd, lon, hellp, jd, dut1, dat, 
         jdtdb, jdtdbfrac = stu.convtime(year, mon, day, hr, minute,
                                     sec + dtsec, timezone, dut1, dat)
         jdut1new = jdut1 + jdut1frac
-        rsat_eci_new, vrat_eci_new = obu.pkepler(rsat_eci_initial,vsat_eci_initial,dtsec, 0.0, 0.0)
-        rsat_ecef_new, vsat_ecef_new ,_ = sc.eci2ecef(rsat_eci_new, vrat_eci_new, asat_eci, ttt, jdut1new, lod, xp, yp, eqeterms, ddpsi, ddeps)
+        rsat_eci_new, vsat_eci_new = obu.pkepler(rsat_eci_initial,vsat_eci_initial,dtsec, 0.0, 0.0)
+        # rsat_eci_new, vsat_eci_new, _ = obu.kepler(rsat_eci_initial,vsat_eci_initial,dtsec)
+
+        rsat_ecef_new, vsat_ecef_new ,_ = sc.eci2ecef(rsat_eci_new, vsat_eci_new, asat_eci, ttt, jdut1new, lod, xp, yp, eqeterms, ddpsi, ddeps)
         rsat_ecef_array[i] = rsat_ecef_new
 
         rhosat_ecef = rsat_ecef_new - rsite_ecef
@@ -122,6 +125,7 @@ def riset(rsat_eci_initial, vsat_eci_initial, latgd, lon, hellp, jd, dut1, dat, 
         rsatmag = smu.mag(rsat_ecef_new)
         fel_high[i] = (np.arccos(np.cos(el)/rsatmag) - el) - (np.arccos(np.cos(ellim_high)/rsatmag) - ellim_high)
         fel_low[i] = (np.arccos(np.cos(el)/rsatmag) - el) - (np.arccos(np.cos(ellim_low)/rsatmag) - ellim_low)
+        rho, az, el, drho, daz, del_ = sc.sez2razel(rhosat_sez_array[i], drhosat_sez_array[i])
 
     rho, az, el, drho, daz, del_ = sc.sez2razel(rhosat_sez_array[0], drhosat_sez_array[0])
     if rho <= rholim:
@@ -154,11 +158,11 @@ def riset(rsat_eci_initial, vsat_eci_initial, latgd, lon, hellp, jd, dut1, dat, 
     minfound = False
     full_az = False
     full_el = False
-    
+
     #full 360 degree view check
     if (0.0 - azlim_low) < small and (2*math.pi - azlim_high) < small:
         full_az = True
-    
+
     if (-math.pi - ellim_low) < small and (math.pi - ellim_high) < small:
         full_el = True
 
@@ -179,6 +183,7 @@ def riset(rsat_eci_initial, vsat_eci_initial, latgd, lon, hellp, jd, dut1, dat, 
         if not(full_el):
             minfound4, rootf4, funrate4 = smu.quartbln(fel_high[i],fel_high[i+1],fel_high[i+2],fel_high[i+3],fel_high[i+4],fel_high[i+5])
             minfound5, rootf5, funrate5 = smu.quartbln(fel_low[i],fel_low[i+1],fel_low[i+2],fel_low[i+3],fel_low[i+4],fel_low[i+5])
+            # print(fel_low[i],fel_low[i+1],fel_low[i+2],fel_low[i+3],fel_low[i+4],fel_low[i+5])
 
         if minfound1:
             minfound = True
@@ -186,17 +191,15 @@ def riset(rsat_eci_initial, vsat_eci_initial, latgd, lon, hellp, jd, dut1, dat, 
             print('root 1: ', rootf1)
             # print('fundrate 1: ', funrate1)
         if minfound2:
-            print('faz_high: ',faz_high[i],faz_high[i+1],faz_high[i+2],faz_high[i+3],faz_high[i+4],faz_high[i+5])
             minfound = True
             combined_root = rootf2
             print('root 2: ', rootf2)
-            # print('fundrate 2: ', funrate2)
+            print('fundrate 2: ', funrate2)
         if minfound3:
-            print('faz_low: ',faz_low[i],faz_low[i+1],faz_low[i+2],faz_low[i+3],faz_low[i+4],faz_low[i+5])
             minfound = True
             combined_root = rootf3
             print('root 3: ', rootf3)
-            # print('fundrate 3: ', funrate3)
+            print('fundrate 3: ', funrate3)
         if minfound4:
             minfound = True
             combined_root = rootf4
@@ -217,10 +220,20 @@ def riset(rsat_eci_initial, vsat_eci_initial, latgd, lon, hellp, jd, dut1, dat, 
             rhosat_sez = np.array([rhosat_s,rhosat_e,rhosat_z])
             drhosat_sez = np.array([drhosat_s,drhosat_e,drhosat_z])
 
-            if minfound2:
-                az_rootvalid = azse_signcheck(azlim_low, azlim_high, rhosat_sez, 'h')
+            # Angles are seperated at 180 degrees.
+            if minfound2 and minfound3:
+                az_rootvalid = True
+                if azse_signcheck(azlim_low, azlim_high, rhosat_sez, 'high'):
+                    print('high azimuth')
+                    minfound3 = False
+                elif azse_signcheck(azlim_low, azlim_high, rhosat_sez, 'low'):
+                    print('low azimuth')
+                    minfound2 =  False
+
+            elif minfound2:
+                az_rootvalid = azse_signcheck(azlim_low, azlim_high, rhosat_sez, 'high')
             elif minfound3:
-                az_rootvalid = azse_signcheck(azlim_low, azlim_high, rhosat_sez, 'l')
+                az_rootvalid = azse_signcheck(azlim_low, azlim_high, rhosat_sez, 'low')
 
             if az_rootvalid:
                 rho,az,el, drho, daz,del_ = sc.sez2razel(rhosat_sez,drhosat_sez)
@@ -236,12 +249,16 @@ def riset(rsat_eci_initial, vsat_eci_initial, latgd, lon, hellp, jd, dut1, dat, 
                     range_inview = False
 
                 if minfound2 and daz < 0:
+                    print('here 1')
                     az_inview = True
                 elif minfound3 and daz > 0:
+                    print('here 2')
                     az_inview = True
                 elif not(minfound2 or minfound3) and (az >= azlim_low and az <= azlim_high):
+                    print('here 3')
                     az_inview = True
                 elif full_az:
+                    print('here 4')
                     az_inview = True
                 else:
                     az_inview = False
@@ -264,7 +281,8 @@ def riset(rsat_eci_initial, vsat_eci_initial, latgd, lon, hellp, jd, dut1, dat, 
                 print('el view: ', el_inview)
                 print('range view: ', range_inview)
                 print('az: ', az)
-                # print('el: ', el)
+                print('daz: ', daz)
+                print('el: ', el)
                 # print('range: ', rho)
 
                 if newview == True and newview != oldview:
@@ -300,25 +318,45 @@ def riset(rsat_eci_initial, vsat_eci_initial, latgd, lon, hellp, jd, dut1, dat, 
 
 if __name__ == '__main__':
 
-    # Vernal Equinox of year 2000
+    # # Vernal Equinox of year 2000
+    # timezone = 0
+    # year = 2000
+    # mon = 3
+    # day = 20
+    # hr = 7 #UTC
+    # minute = 35
+    # sec = 0.0
+    # utc = sec
+
+    # # source: https://hpiers.obspm.fr/iers/series/opa/eopc04
+    # xp = 0.073758
+    # yp = 0.353317
+    # dut1 = 0.2847777
+    # ut1 = utc + dut1
+    # lod = 0.0010450
+    # ddpsi = -0.048346
+    # ddeps = -0.005581
+    # dat = 32
+
+    # Vernal Equinox of year 1992
     timezone = 0
-    year = 2000
+    year = 1992
     mon = 3
     day = 20
-    hr = 7 #UTC
-    minute = 35
+    hr = 8 #UTC
+    minute = 47
     sec = 0.0
     utc = sec
 
     # source: https://hpiers.obspm.fr/iers/series/opa/eopc04
-    xp = 0.073758
-    yp = 0.353317
-    dut1 = 0.2847777
+    xp = -0.054482
+    yp = 0.137482
+    dut1 = -0.3234685
     ut1 = utc + dut1
-    lod = 0.0010450
-    ddpsi = -0.048346
-    ddeps = -0.005581
-    dat = 32
+    lod = 0.033059
+    ddpsi = -0.009127
+    ddeps = -0.004187
+    dat = 27
 
     jdut1, jdut1frac = stu.jday(year, mon, day, hr, minute, ut1)
 
@@ -326,17 +364,55 @@ if __name__ == '__main__':
     latgd = 39.007 * deg2rad
     lon = -104.883 * deg2rad
     hellp = 2.918
+
+    # Limits depend on antenna site survey. Values below are arbitrary.
     rholim = 100000 #km
     # Superimposed circle over actual azimuth/elevation limits
-    azlim_low = 0*deg2rad
-    azlim_high = 180*deg2rad
+    azlim_low = 0.0*deg2rad
+    azlim_high = 360*deg2rad
     ellim_low = 30*deg2rad
     ellim_high = 90*deg2rad
+    objectnum = 8
 
-    # Sat Object 8 data
-    n = 12.41552416 * revday2radsec
-    ecc = 0.0036498
-    incl = 74.0186 * deg2rad
+    # Sat Objects 1, 2 (?), 4, and 7 are not visible to site
+    match objectnum:
+        case 1:
+            n = 1.00272141 * revday2radsec
+            ecc = 0.0000032
+            incl = 0.0956 * deg2rad
+        case 2:
+            n = 8.36589235 * revday2radsec
+            ecc = 0.0080158
+            incl = 90.0175 * deg2rad
+        case 3:
+            n = 0.24891961 * revday2radsec
+            ecc = 0.9363060
+            incl = 64.9874 * deg2rad
+        case 4:
+            n = 0.21467209 * revday2radsec
+            ecc = 0.0668128
+            incl = 57.3500 * deg2rad
+        case 5:
+            n = 13.37659679 * revday2radsec
+            ecc = 0.0145072
+            incl = 90.2619 * deg2rad
+        case 6:
+            n = 16.09769232 * revday2radsec
+            ecc = 0.0078742
+            incl = 82.8709 * deg2rad
+        case 7:
+            n = 1.00271920 * revday2radsec
+            ecc = 0.0003109
+            incl = 0.0099 * deg2rad
+        case 8:
+            n = 12.41552416 * revday2radsec
+            ecc = 0.0036498
+            incl = 74.0186 * deg2rad
+        case 9:
+            n = 13.84150848 * revday2radsec
+            ecc = 0.0048964
+            incl = 144.6414 * deg2rad
+
     omega = 0.0
     argp = 0.0
     m = 0.0
@@ -348,11 +424,13 @@ if __name__ == '__main__':
 
     riset_time, riset_az, riset_el = riset(rsat_eci, vsat_eci, latgd, lon, hellp, jdut1 + jdut1frac, dut1, dat, lod, xp, yp, ddpsi, ddeps, rholim, azlim_low, azlim_high, ellim_low, ellim_high)
 
-
-    for i in range(len(riset_time)):
-        print(f'rise time: {riset_time[i][0]}')
-        print(f'rise az  : {riset_az[i][0]}')
-        print(f'rise el  : {riset_el[i][0]} \n')
-        print(f'set time: {riset_time[i][1]}')
-        print(f'set az  : {riset_az[i][1]}')
-        print(f'set el  : {riset_el[i][1]}\n\n')
+    if len(riset_time) == 0:
+        print('Satellite is not visible to site')
+    else:
+        for i in range(len(riset_time)):
+            print(f'rise time: {riset_time[i][0]}')
+            print(f'rise az  : {riset_az[i][0]}')
+            print(f'rise el  : {riset_el[i][0]} \n')
+            print(f'set time: {riset_time[i][1]}')
+            print(f'set az  : {riset_az[i][1]}')
+            print(f'set el  : {riset_el[i][1]}\n\n')
