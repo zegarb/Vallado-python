484d858b9a49c68e01ec32056e2a0825096bac5e
zegarb <zgarbarino@gmail.com>
2024-08-01 10:11:27 -0400

    unscentedkalman numpy notes/fix

--- dungeon/unscentedkalman.py
+++ dungeon/unscentedkalman.py
@@ -2,6 +2,13 @@ import numpy as np
 import math
 
 # from the book page 798: 4th edition, or 814: 5th edition
+
+# Note: numpy does NOT like 'vertical slice' arrays (shape(x, 1)).
+# it will automatically convert a vertical slice to a 1D array
+# (s[0:3, i], pts[0:6, i]), and will expect a 1D array when
+# assigning to a vertical slice (sigmapts[0:3, jj], y[0:6, i]).
+# Trying to assign a shape(3, 1) array to sigmapts[0:3, jj] will crash! -zeg
+
 def unscentedkalman(reci: np.ndarray, veci: np.ndarray, covariance: np.ndarray):
     s = math.sqrt(6) * np.linalg.cholesky(covariance)
     sigmapts = np.zeros([6, 12])
@@ -9,6 +16,7 @@ def unscentedkalman(reci: np.ndarray, veci: np.ndarray, covariance: np.ndarray):
     reci = reci.flatten()
     veci = veci.flatten()
     for i in range(6):
+        # jj modified for starting at index 0 instead of 1
         jj = i * 2
         sigmapts[0:3, jj] = reci + s[0:3, i]
         sigmapts[3:6, jj] = veci + s[3:6, i]
@@ -25,7 +33,7 @@ def unscentedreassemble(pts: np.ndarray):
 
     y = np.zeros([6, 12])
     for i in range(12):
-        y[0:6] = pts[0:6, i] - yu[0:6]
+        y[0:6, i] = pts[0:6, i] - yu
     tmp = (y @ y.T) / 12
     cov = (tmp + tmp.T) / 2
     return cov
